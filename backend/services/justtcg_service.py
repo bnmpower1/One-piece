75efe0b9a5ad29d11a6d9eb4c3c498ccb371254d
bnmpower1 <bnmpowerislove@gmail.com>
2026-06-09 21:59:25 -0700

    remove justtcg service

--- backend/services/justtcg_service.py
+++
@@ -1,6 +0,0 @@
-from services.ebay_service import fetch_ebay_prices
-from services.sheets_service import CardRecord
-
-
-def fetch_justtcg_price(card: CardRecord) -> dict[str, float] | None:
-    return fetch_ebay_prices(card.card_number, card.name, card.variant)
